3a5c619a7f498a58b69f020f8a26f3675e74bbec
youssef7osso <54089843+youssef7osso@users.noreply.github.com>
2019-08-29 04:11:15 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("611799502440890368")
+setInterval(function() {
+channel.send(`سبام ليووسف المعلم فشخ اوي وجامد جدا جدا جدا وبيفهم في كل حاجة `);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
